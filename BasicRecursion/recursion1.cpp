85be04a01c5c4506708a3b83762665ff50ea5210
coder-sid-byter <siddharthmishra2k24@gmail.com>
2025-05-25 09:18:54 +0530

    printing name without global variable

--- BasicRecursion/recursion1.cpp
+++ BasicRecursion/recursion1.cpp
@@ -5,11 +5,27 @@ void name5(){
     
     if(cnt <5) cout<<"siddharth"<<endl;
 
-    cnt++;
+    cnt++;  
     name5();
 }
 
+
+void printname(int i,int n){
+    if(i>n){
+        return;
+    }
+    else{
+        cout<<"sid"<<" ";
+    }
+    printname(i+1,n);
+}
+
+
 int main(){
-    name5();
+    // name5();
+    int n;
+    cout<<"enter n value"<<endl;
+    cin>>n;
+    printname(1,n);
     return 0;
 }
