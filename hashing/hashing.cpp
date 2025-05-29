1892964ff4380394481b4dae1593367e384dfbab
coder-sid-byter <siddharthmishra2k24@gmail.com>
2025-05-29 09:21:22 +0530

    MapHashing

--- hashing/hashing.cpp
+++ hashing/hashing.cpp
@@ -22,6 +22,34 @@ void hashingAlpha(){
 
 }
 
+void Maphashing(){
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"enter the array element "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    map<int,int> mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+
+
+    int q;
+    cout<<"enter the value of q";
+    cin>>q;
+    while(q--){
+        int number;
+        cout<<"Enter the number :"<<endl;
+        cin>>number;
+
+        cout<<mpp[number]<<endl;
+    }
+}
+
 int main(){
     /*int n;
     cout<<"enter n"<<endl;
@@ -50,7 +78,10 @@ int main(){
     }
         */
 
-    hashingAlpha();
+    // hashingAlpha();
+    Maphashing();
 }
+
+
 
 
