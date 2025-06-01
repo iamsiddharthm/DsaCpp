c33d4cea4d0f9f626172c02bf78ce5f390cfad5d
coder-sid-byter <siddharthmishra2k24@gmail.com>
2025-06-01 11:02:39 +0530

    edited bubble sort

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -16,21 +16,12 @@ void selection_sort(int arr[],int n){
 }
 
 void bubble_sort(int arr[],int n){
-    //bubble sort :push the max element to the last 
-    for(int i=n-1;i>0;i--){
-        int didSwap=0;
-        for(int j=0;j<=i-1;j++){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
-                int temp=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=temp;
-                didSwap=1;
+                swap(arr[j],arr[j+1]);
             }
         }
-        if(didSwap=1){
-            break;
-        }
-
     }
 }
 
