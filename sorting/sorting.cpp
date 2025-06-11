8847d61a631e985c9917f28d6a56315a1a3ebd76
coder-sid-byter <siddharthmishra2k24@gmail.com>
2025-06-11 18:51:56 +0530

    insetion sort

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -28,17 +28,26 @@ void bubble_sort(int arr[],int n){
 void insertion_sort(int arr[],int n){
     //insertion sort : take an eleement and place it in correct order
     for(int i=0;i<n;i++){
-        int curr=arr[i];
-        int prev=i-1;
-        while(prev>=0 && arr[prev]>curr){
-            arr[prev+1]=arr[prev];
-            prev--;
-        }
-
-        arr[prev+1]=curr;
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]) 
+        swap(arr[j-1],arr[j]) ;
+        j--;
     }
 }
+//insertion sort with shifting :-
+/*
+for (int i = 1; i < n; i++) {
+    int key = arr[i];
+    int j = i - 1;
 
+    while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j]; // shift elements to the right
+        j--;
+    }
+    arr[j + 1] = key; // insert at the correct position
+}
+
+*/
 int main(){
     int n;
     cout<<"enter the value of n"<<endl;
@@ -47,6 +56,7 @@ int main(){
     for(int i=0;i<n;i++) cin>>arr[i];
 
     insertion_sort(arr,n);
+
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
