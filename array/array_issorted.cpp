01ec309f0d5ae1599d5ecbcf940328453bec2644
coder-sid-byter <siddharthmishra2k24@gmail.com>
2025-10-05 10:49:35 +0530

    reversing an array and checking is sorted

--- array/array_issorted.cpp
+++ array/array_issorted.cpp
@@ -2,17 +2,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool is_sorted(int n,vector<int> a){
+bool is_sorted(int n,int a[]){
     for(int i=1;i<n;i++){
         if(a[i]>=a[i-1]){
 
-        return false;
-    }}
+        
+    }
+    else return false;}
     return true;
 }
 
 //first element -to-> second element are u greater or equal to 2nd elemnt if yes do nothing if no return false
 int main(){
     int arr[6]={1,2,3,4,5,6};
+    if(is_sorted(6,arr)){
+        cout<<"sorted"<<endl;
+    }
+    else{
+        cout<<"not sorted"<<endl;
+    };
     return 0;
 }
